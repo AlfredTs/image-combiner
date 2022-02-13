5aa753870ff3524f7cf06c52e62ef5624907b62d
sioppz <sioppz@pop-os.localdomain>
2022-02-14 01:35:31 +0400

    Added HUE shift capability

--- combiner.py
+++ combiner.py
@@ -1,15 +1,41 @@
 import os
-from tqdm import tqdm
 import random
+import colorsys
+import numpy as np
+from tqdm import tqdm
 from PIL import Image, ImageDraw, ImageFilter
+
+#hue shifting stuff
+rgb_to_hsv = np.vectorize(colorsys.rgb_to_hsv)
+hsv_to_rgb = np.vectorize(colorsys.hsv_to_rgb)
+
+def shift_hue(arr, hout):
+    r, g, b, a = np.rollaxis(arr, axis=-1)
+    h, s, v = rgb_to_hsv(r, g, b)
+    h = hout
+    r, g, b = hsv_to_rgb(h, s, v)
+    arr = np.dstack((r, g, b, a))
+    return arr
+
+def colorize(image, hue):
+    """
+    Colorize PIL image `original` with the given
+    `hue` (hue within 0-360); returns another PIL image.
+    """
+    img = image.convert('RGBA')
+    arr = np.array(np.asarray(img).astype('float'))
+    new_img = Image.fromarray(shift_hue(arr, hue/360.).astype('uint8'), 'RGBA')
+
+    return new_img
 
 
 directory = sorted(os.listdir())
 layers = []
+hueTag = '_hue'
 
 #read all the files
 for f in directory:
-	if os.path.isdir(f):
+	if os.path.isdir(f) and f[0]!='.':
 		layers.append([f+'/'+x for x in sorted(os.listdir(f))])
 
 
@@ -39,10 +65,19 @@ for i in range(0,shuffles):
 for i in tqdm(range(len(combinations))):
 	comb = combinations[i]
 	mix = Image.open(comb[0])
+	shifts = []
 	for f in comb[1:]:
-		mix = Image.alpha_composite(mix,Image.open(f))
+		img = Image.open(f)
+
+		if hueTag in f:
+			arr = np.array(img)
+			hueShift = random.random()
+			img = Image.fromarray(shift_hue(arr,hueShift),'RGBA')
+			shifts.append(str(hueShift))
+		
+		mix = Image.alpha_composite(mix,img)
 	
-	mix.save('output_'+str(hash('-'.join(comb)))+'.png',format='png')
+	mix.save('output_'+str(hash('-'.join(comb)+'_'.join(shifts)))+'.png',format='png')
 
 
 
